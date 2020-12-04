3f386a38df15804dd2eee2abedf3f6793e26d0a5
Robert Michaud <robert.cn.michaud@hotmail.com>
2020-12-04 17:59:45 -0500

    merge changes, restored to previous

--- financials.php
+++ financials.php
@@ -33,29 +33,29 @@
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_1_owned"]}</td>";
-    ?>
+?>
 <br>
 <b> Total monthly fees paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(default_monthly_payment) AS building_1_default_yearly_sum FROM transaction_record WHERE user_id <= 10";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_1_default_yearly_sum"]}</td>";
-    ?>  
+?>  
 	<br>
 	<b> Total maintenance costs paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(total_cost) AS building_1_maintenance FROM maintenance WHERE building_id = 1";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_1_maintenance"]}</td>";
-    ?>  
+?>  
 	
 	  <div class=\"btn-group mr-2\">
                 <a href=\"building_1_transactions.php?uid={$row["user_id"]}\"><button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">Full Yearly Report</button></a>
@@ -74,29 +74,29 @@
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_2_owned"]}</td>";
-    ?>
+?>
 <br>
 <b> Total monthly fees paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(default_monthly_payment) AS building_2_default_yearly_sum FROM transaction_record WHERE (10 < user_id AND user_id <= 20)";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_2_default_yearly_sum"]}</td>";
-    ?>  
+?>  
 	<br>
 	<b> Total maintenance costs paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(total_cost) AS building_2_maintenance FROM maintenance WHERE building_id = 2";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_2_maintenance"]}</td>";
-    ?>  
+?>  
 	
 	  <div class=\"btn-group mr-2\">
                 <a href=\"building_2_transactions.php?uid={$row["user_id"]}\"><button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">Full Yearly Report</button></a>
@@ -113,29 +113,29 @@
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_3_owned"]}</td>";
-    ?>
+?>
 <br>
 <b> Total monthly fees paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(default_monthly_payment) AS building_3_default_yearly_sum FROM transaction_record WHERE (20 < user_id AND user_id <= 30)";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_3_default_yearly_sum"]}</td>";
-    ?>  
+?>  
 	<br>
 	<b> Total maintenance costs paid this year:</b>
 </select>
-    <?php
+<?php
         require_once "includes/db_handler_inc.php";
         require_once "includes/functions_inc.php";
 		$sql = "SELECT SUM(total_cost) AS building_3_maintenance FROM maintenance WHERE building_id = 3";
 		$query_result = mysqli_query($conn, $sql);
 		$row = mysqli_fetch_assoc($query_result);
 		  echo "<td>{$row["building_3_maintenance"]}</td>";
-    ?>  
+?>  
 	
 	  <div class=\"btn-group mr-2\">
                 <a href=\"building_3_transactions.php?uid={$row["user_id"]}\"><button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">Full Yearly Report</button></a>
