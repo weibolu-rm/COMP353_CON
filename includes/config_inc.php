cb46a07764e94b9c2bb8a51f111b1542548e7593
Robert Michaud <robert.cn.michaud@hotmail.com>
2020-12-14 20:50:03 -0500

    removed error reporting for deployment

--- includes/config_inc.php
+++ includes/config_inc.php
@@ -29,5 +29,5 @@
 
     date_default_timezone_set("America/New_York");
     // TODO: REMOVE BEFORE DEPLOYMENT
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    // error_reporting(0);
+    // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    error_reporting(0); // for deployment, don't want to somehow leak something to the end user
